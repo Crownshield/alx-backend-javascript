2996c7489a9ce91eb6eff4b2f22cc2bfc096e4bc
Crownshield <adefisayoadeniji@gmail.com>
2023-08-02 21:54:18 +0000

    0x04-TypeScript

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,42 +1,70 @@
-// Write an interface named Student that accepts the following elements: firstName(string), lastName(string), age(number), and location(string)
-// Create two students, and create an array named studentsList containing the two variables
-// Using Vanilla Javascript, render a table and for each elements in the array, append a new row to the table
-// Each row should contain the first name of the student and the location
 
-// Define the Student interface
-interface Student {
-	firstName: string;
-	lastName: string;
-	age: number;
-	location: string;
+/**
+ * Write an interface named Studentthat accepts the
+ * following elements:
+ * - firstName(string),
+ * - lastName(string),
+ * - age(number),
+ * - location(string)
+ */
+
+export interface Student {
+  firstName: string,
+  lastName: string,
+  age: number,
+  location: string,
 }
 
-// Create two student objects
-const firstStudent: Student = {
-	firstName: "Obed",
-	lastName: "Ehoneah",
-	age: 97,
-	location: "Accra"
+/**
+ * Create two students, and create an array named studentsList
+ * containing the two variables
+ */
+
+const student1: Student = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 25,
+  location: 'Nairobi',
+};
+
+const student2: Student = {
+  firstName: 'Alex',
+  lastName: 'Sanchez',
+  age: 29,
+  location: 'Cairo',
+};
+
+const studentsList: Array<Student> = [
+  student1,
+  student2,
+];
+
+
+/**
+ * - Using Vanilla Javascript, render a table and for each elements in the array,
+ * append a new row to the table
+ * 
+ * - Each row should contain the first name of the student and the location
+ */
+
+export const renderTable = (studentsList: Array<Student>): void =>  {
+  // create table tag
+  const table = document.createElement('table');
+  const headRow = document.createElement('tr');
+  table.insertAdjacentElement('beforeend', headRow);
+
+  // insert headers
+  headRow.insertAdjacentHTML('beforeend', '<th>FirstName</th>');
+  headRow.insertAdjacentHTML('beforeend', '<th>Location</th>');
+
+  for (const student of studentsList) {
+    const studentRow = document.createElement('tr')
+    studentRow.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
+    studentRow.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
+    table.insertAdjacentElement('beforeend', studentRow);
+  }
+
+  document.body.insertAdjacentElement('beforeend', table);
 }
 
-const secondStudent: Student = {
-	firstName: "Frank",
-	lastName: "Ehoneah",
-	age: 79,
-	location: "Kumasi"
-}
-
-// Create an array of students
-const studentsList = [firstStudent, secondStudent];
-
-const table = document.getElementById('studentTable') as HTMLTableElement;
-
-// Create a new row for each student and append it to the table
-studentsList.forEach((student) => {
-	const row = table.insertRow();
-	const firstNameCell = row.insertCell(0);
-	const locationCell = row.insertCell(1);
-
-	firstNameCell.innerHTML = student.firstName;
-	locationCell.innerHTML = student.location;
-})
+renderTable(studentsList);
